5fbb55fdab61f15041f6c7d99c2d4cd5b957ff92
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-04-20 05:35:29 +0630

    out keyword

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,14 @@
 
 
-class Team<T:Player>(val name: String, val players: MutableList<T>){
+//class Team<T:Player>(val name: String, val players: MutableList<T>){
+class Team<T:Player>(val name: String, val players: MutableList<out T>){
     fun addPlayer(player: T){
         if(players.contains(player)){
 //            println("Player: ${(player as Player).name} is already in the team ${this.name}")
             println("Player: ${player.name} is already in the team ${this.name}")
         }
         else{
-            players.add(player)
+            players.add(player as Nothing)
 //            println("Player: ${(player as Player).name} was added in the team ${this.name}")
             println("Player: ${player.name} was added in the team ${this.name}")
         }
