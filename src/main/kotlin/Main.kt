c2625e3ebe57afe8489d376970ac55f57cee48bc
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-04-22 06:26:15 +0630

    where 2 upper bounds

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,6 @@
 
 
-//class Team<T:Player>(val name: String, val players: MutableList<T>){
-
-// convariances
-//class Team<T:Player>(val name: String, val players: MutableList<out T>){
-
-
-// contravariance
-class Team<T:Player>(val name: String, val players: MutableList<in  T>){
+class Team<T>(val name: String, val players: MutableList<in  T>)where T:Player, T:Listener{
     fun addPlayer(player: T){
         if(players.contains(player)){
 //            println("Player: ${(player as Player).name} is already in the team ${this.name}")
@@ -21,31 +14,21 @@ class Team<T:Player>(val name: String, val players: MutableList<in  T>){
     }
 }
 
+interface Listener{
+    fun listen(){}
+}
+
+fun <T> addPlayer(player:T) where T:Player, T:Listener{}
+
+
 fun main(args: Array<String>) {
-    val mixedList= mutableListOf(1,2,360,'a','b','c',"Hello","World")
-    val spec=getSpecificTypes<Int>(mixedList)
-    for(element in spec){
-        println(element)
-    }
+    val gamesTeam=Team<FootballPlayer>(
+        "Football Team",mutableListOf()
+    )
 }
-
-inline fun <reified T> getSpecificTypes(list: List<Any>):List<T>{
-    val specificList= mutableListOf<T>()
-    for(element in list){
-        if(element is T){
-            specificList.add(element)
-        }
-    }
-    return  specificList
-}
-
-
-fun collectionGeneric(){
-    val numbers:Array<Int> = arrayOf<Int>(1,2,3,4,5,6)
-}
-
 open class Player(val name:String)
-class FootballPlayer(name:String):Player(name)
+// here
+class FootballPlayer(name:String):Player(name),Listener
 class BaseballPlayer(name: String):Player(name)
 open class GamesPlayer(name:String):Player(name)
 
